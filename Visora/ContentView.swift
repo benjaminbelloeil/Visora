870e26c240e80e6ce84ae66c7ac793a3a2fd79d8
Benjamin Belloeil <benjaminbelloeil@outlook.com>
2025-11-07 10:33:56 +0100

    Initial Commit

---
+++ Visora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Visora
+//
+//  Created by Benjamin Belloeil on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Visora/VisoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisoraApp.swift
+//  Visora
+//
+//  Created by Benjamin Belloeil on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VisoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
